af2f6f7b3cbca1775fd4de1bba72179acb2391a8
Brian Morin <brianrossmorin@gmail.com>
2026-01-11 13:27:44 -0800

    promises

--- src/async/promises.js
+++ src/async/promises.js
@@ -20,10 +20,10 @@ const getDataPromiseChain = () => {
   return fakeApiCall({ name: "Heather" }).then((res) => {
     retVal.name = res.data.name;
 
-    fakeApiCall({ job: "code monkey" }).then((res) => {
+    return fakeApiCall({ job: "code monkey" }).then((res) => {
       retVal.job = res.data.job;
 
-      fakeApiCall({ age: 420 }).then((res) => {
+      return fakeApiCall({ age: 420 }).then((res) => {
         retVal.age = res.data.age;
         return retVal;
       });
@@ -34,10 +34,16 @@ const getDataPromiseChain = () => {
 const getData = async () => {
   // use the async/await pattern to refactor the code above and make the tests pass
   // you should return an object in the same shape as the `retVal` above
+
+  return {
+    name: (await fakeApiCall({ name: "Heather" })).data.name,
+    job: (await fakeApiCall({ job: "code monkey" })).data.job,
+    age: (await fakeApiCall({ age: 420 })).data.age,
+  };
 };
 
 const handleMultiplePromises = async (promises = []) => {
-  // use promise.all to resolve multiple promises
+  return await Promise.all(promises);
 };
 
 /**
@@ -45,8 +51,33 @@ const handleMultiplePromises = async (promises = []) => {
  * @param {Array} promises an array of promises
  * @returns {Array} an array of responses returned from a set of promises
  */
-const myPromiseAll = async (promises = []) => {
-  // DO NOT use Promise.all
+const myPromiseAll = (promises = []) => {
+  // this runs all the promises in parallel and resolves
+  // when all the promises have resolved or any errors
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completedCount = 0;
+
+    if (promises.length === 0) {
+      resolve(results);
+      return;
+    }
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise).then(
+        (value) => {
+          results[index] = value;
+          completedCount++;
+          if (completedCount === promises.length) {
+            resolve(results);
+          }
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  });
 };
 
 module.exports = {
