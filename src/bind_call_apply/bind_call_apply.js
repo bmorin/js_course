72d89d6ea8c29160595fe7f128ec11628635aa4e
Brian Morin <brianrossmorin@gmail.com>
2026-01-11 12:11:02 -0800

    bind_call_apply

--- src/bind_call_apply/bind_call_apply.js
+++ src/bind_call_apply/bind_call_apply.js
@@ -4,7 +4,7 @@
  * @returns {string} `Hey my name is <person name>
  */
 function personGreeting() {
-  // your code here
+  return `Hey my name is ${this.name}`;
 }
 
 /**
@@ -12,10 +12,10 @@ function personGreeting() {
  *
  * @param {string} like1
  * @param {string} like2
- * @returns {string} My name is {name} I am a {job} like {like1} and {like2}
+ * @returns {string} My name is {name} and I am a {job} and like {like1} and {like2}
  */
 function personSmallTalk(like1, like2) {
-  // your code here
+  return `My name is ${this.name} and I am a ${this.job} and like ${like1} and ${like2}`;
 }
 
 /**
@@ -25,17 +25,25 @@ function personSmallTalk(like1, like2) {
  * @returns {string} Hey my name is {name} and I like {hobbies}
  */
 function listHobbies(...hobbies) {
-  // your code here
+  // Join hobbies with commas and 'and' before the last hobby
+  hobbyList = hobbies.length <= 2
+    ? hobbies.join(' and ')
+    : `${hobbies.slice(0, -1).join(', ')} and ${hobbies.at(-1)}`;
+
+  return `Hey my name is ${this.name} and I like ${hobbyList}`;
 }
 
 // HINT: leverage the `call` method to make the following functions work as expected
 
 Function.prototype.myBind = function (context, ...args) {
-  // your code here
+  const originalFunc = this;
+  return function (...newArgs) {
+    return originalFunc.call(context, ...args, ...newArgs);
+  };
 };
 
 Function.prototype.myApply = function (context, args = []) {
-  // your code here
+  return this.call(context, ...args);
 };
 
 module.exports = {
