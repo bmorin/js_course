b3b202f2eab7d2bd68a4077bf0c7d47c9fdd1c33
Brian Morin <brianrossmorin@gmail.com>
2026-01-11 12:48:34 -0800

    map_filter_reduce

--- src/map_filter_reduce/my_array_methods.js
+++ src/map_filter_reduce/my_array_methods.js
@@ -3,23 +3,43 @@
  * PLEASE DO NOT USE THE BUILT IN METHODS FOR THE FOLLOWING EXERCISES
  */
 Array.prototype.myFilter = function (callBack) {
-  //add your code here
+  const result = [];
+  for (const item of this) {
+    if (callBack(item)) {
+      result.push(item);
+    }
+  }
+  return result;
 };
 
 Array.prototype.myMap = function (callBack) {
-  //add your code here
+  const result = [];
+  for (const item of this) {
+    result.push(callBack(item));
+  }
+  return result;
 };
 
 Array.prototype.myEvery = function (callBack) {
-  //add your code here
+  for (const item of this) {
+    if (!callBack(item)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 Array.prototype.mySome = function (callBack) {
-  //add your code here
+  for (const item of this) {
+    if (callBack(item)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 const addAllNumbers = (arr) => {
-  //add your code here using reduce
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
 module.exports = {
