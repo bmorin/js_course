2a9ab440021adfda32a57ed5cff58b1506d76dab
Brian Morin <brianrossmorin@gmail.com>
2026-01-12 23:09:13 -0800

    sorting searching

--- src/sorting_searching_algos/sorting_searching.js
+++ src/sorting_searching_algos/sorting_searching.js
@@ -6,7 +6,19 @@
  */
 
 const binarySearch = (arr, target) => {
-	// TODO: implement binary search
+	let left = 0;
+	let right = arr.length - 1;
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		if (arr[mid] === target) {
+			return mid;
+		} else if (arr[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return -1;
 };
 
 /**
@@ -16,7 +28,26 @@ const binarySearch = (arr, target) => {
  */
 
 const mergeSort = (arr) => {
-	// TODO: implement merge sort
+	if (arr.length <= 1) {
+		return arr;
+	}
+	const mid = Math.floor(arr.length / 2);
+	const left = mergeSort(arr.slice(0, mid));
+	const right = mergeSort(arr.slice(mid));
+
+	const merged = [];
+	let i = 0;
+	let j = 0;
+	while (i < left.length && j < right.length) {
+		if (left[i] < right[j]) {
+			merged.push(left[i]);
+			i++;
+		} else {
+			merged.push(right[j]);
+			j++;
+		}
+	}
+	return [...merged, ...left.slice(i), ...right.slice(j)];
 };
 
 /**
@@ -26,7 +57,20 @@ const mergeSort = (arr) => {
  */
 
 const quickSort = (arr) => {
-	// TODO: implement quick sort
+	if (arr.length <= 1) {
+		return arr;
+	}
+	const pivot = arr[arr.length - 1];
+	const left = [];
+	const right = [];
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] < pivot) {
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	return [...quickSort(left), pivot, ...quickSort(right)];
 };
 
 module.exports = {
